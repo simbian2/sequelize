df4549893500dd828ad9df3c4242edba359fbb1e
seongminNa <hye1209cj@naver.com>
2021-06-03 15:52:08 +0900

    commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const nunjucks = require('nunjucks')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb)=>{
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({storage: storage})
+
+app.set('view engine','html')
+app.use(bodyParser.urlencoded({extended:false}))
+
+nunjucks.configure('views',{
+    express:app,
+})
+
+app.use(express.static('uploads'))
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.post('/file',upload.single('userfile'),(req,res)=>{
+    res.redirect('/')
+})
+
+app.listen(3000,()=>{
+    console.log('server start port 3000')
+})
